be12980943b20e26fffa71ff3ee430eba7238bfd
thanhtuan472k <tuango.472000@gmail.com>
2022-03-03 12:13:15 +0700

    remove git

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/f8_education_dev', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-        });
+        await mongoose.connect(
+            'mongodb+srv://Ngothanhtuan123:Ngothanhtuan123@cluster0.6vlqj.mongodb.net/f8-courses?retryWrites=true&w=majority',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+            },
+        );
         console.log('Connect successfully!!!');
     } catch (error) {
         console.log('Connect failure!!!');
